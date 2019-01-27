e5ee5b2929cb9bd0d062fbbaaddf64e1c6fc3425
Aleksander Seryogin <a.siarohin@gmail.com>
2019-01-28 00:37:24 +0300

    fix: translate content `author of the day` on main page

--- src/components/author-of-day/index.js
+++ src/components/author-of-day/index.js
@@ -1,6 +1,8 @@
+import $ from 'jquery';
 import authorRender from './author-of-day.template';
 import data from '../../data/ru.json';
-import $ from 'jquery';
+import dataEng from '../../data/eng.json';
+import dataBy from '../../data/by.json';
 import './index.scss';
 
 if ($('.content-person').length >= 0 ) {
@@ -8,5 +10,23 @@ if ($('.content-person').length >= 0 ) {
   container.empty();
   const date = new Date();
   const currentId = date.getDate() % data.length;
-  container.append(authorRender(data.find(person => person.id == currentId)));
+
+  const urlParams = new URLSearchParams(window.location.search);
+  const lang = urlParams.get('lang');
+
+  let title = 'Фотограф дня';
+  switch (lang) {
+    case 'en':
+      title = 'Author of the Day';
+      container.append(authorRender(dataEng.find(person => person.id == currentId)));
+      break;
+    case 'by':
+      title = 'Аўтар гэтага дня';
+      container.append(authorRender(dataBy.find(person => person.id == currentId)));
+      break;
+    default:
+      container.append(authorRender(data.find(person => person.id == currentId)));
+  }
+
+$('.random-title-author').text(title);
 }
