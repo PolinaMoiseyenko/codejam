c944e48b8d4de77d4eb3253fa9fc3288db141ac9
Aleksander Seryogin <a.siarohin@gmail.com>
2019-01-27 23:55:33 +0300

    feat: translate `all` content

--- src/components/all/index.js
+++ src/components/all/index.js
@@ -1,8 +1,23 @@
-import listItemRender from './list-item.template';
-import data from './../../data/ru.json'
 import $ from 'jquery';
+import listItemRender from './list-item.template';
+import data from '../../data/ru.json';
+import dataEng from '../../data/eng.json';
+import dataBy from '../../data/by.json';
 
-if ( $('.photographs').length > 0 ) {
+if ($('.photographs').length > 0) {
   const container = $('.photographs-list');
-	data.map(person => container.append(listItemRender(person)) );
+
+  const urlParams = new URLSearchParams(window.location.search);
+  const lang = urlParams.get('lang');
+
+  switch (lang) {
+    case 'en':
+      dataEng.map(person => container.append(listItemRender(person)));
+      break;
+    case 'by':
+      dataBy.map(person => container.append(listItemRender(person)));
+      break;
+    default:
+      data.map(person => container.append(listItemRender(person)));
+  }
 }
