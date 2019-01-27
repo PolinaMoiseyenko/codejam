cf0381755c5eaaf5cd94377561de010aa85f8752
rss-cj-22 <rss.cj.22@gmail.com>
2019-01-27 23:10:32 +0200

    Translate content of the site (#18)
    * feat: add language change for header on main page
    
    * fix: fix problem with language change
    
    * feat: translate `person` content
    
    * feat: add translation of main page
    
    * feat: translate `all` content

--- src/components/leaflet/index.template.js
+++ src/components/leaflet/index.template.js
@@ -1,3 +1,3 @@
 export default `
-<h2 class="content-person-title geo-title">Биография на карте</h2>
+<h2 class="content-person-title geo-title"></h2>
 <div id="map" class="content-person-geo"></div>`;
