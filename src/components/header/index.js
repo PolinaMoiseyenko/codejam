750b4843d283644a9fe70836cad9584ec51a37da
Aleksander Seryogin <a.siarohin@gmail.com>
2019-01-28 20:29:49 +0300

    fix: fix ref error

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -2,7 +2,6 @@ import $ from 'jquery';
 
 import headerRender from './header.template';
 import scrollTo from '../../utils/scrollTo';
-import './header.scss';
 import data from '../../data/interface/translation.json';
 import './index.scss';
 
