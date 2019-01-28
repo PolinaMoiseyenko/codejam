f87c2a16b4c1deccd6918bb605043aef8ac1ac0a
Elena Khlyavskaya <lenach241@gmail.com>
2019-01-28 11:31:53 +0300

    fix: fix translations of components

--- src/components/person/person.template.js
+++ src/components/person/person.template.js
@@ -1,3 +1,5 @@
+import translate from "./../../utils/translate";
+
 export default function(person) {
   return `<section class="content-section content-person-row main-row container d-flex flex-column align-items-center">
                 <section class="row artist-page-content content-person-area about-area">
@@ -6,7 +8,7 @@ export default function(person) {
                     </div>
                     <div class="col-9 artist-intro">
                         <h1 class="content-person-title about-title section-title">${person.name}</h1>
-                        <article class="content-person-item">Lorem, ipsum dolor sit amet consectetur adipisicing elit.</article>
+                               <!--<article class="content-person-item">Lorem, ipsum dolor sit amet consectetur adipisicing elit.</article>-->
                         <!-- Start video overlay -->
                         <div class="overlay-video">
                             <div class="video-wrapper-ext">
@@ -16,13 +18,13 @@ export default function(person) {
                                 </div>
                             </div>
                         </div>
-                        <button data-url="${person.video}" class="content-person-video js-overlay-start btn btn-light mt-3">Видео</button>
+                        <button data-url="${person.video}" class="content-person-video js-overlay-start btn btn-light mt-3">${ translate("Видео") }</button>
                         <!-- End video overlay -->
                     </div>
                 </section>
                 <!-- Right 2/3 -->
                 <section class="content-person-area timeline-area">
-                    <h2 class="content-person-title timeline-title section-title border-bottom border-light mt-5">Timeline</h2>
+                    <h2 class="content-person-title timeline-title section-title border-bottom border-light mt-5">${ translate("История жизни") }</h2>
                     <ul class="timeline">
                     ${person.biography.map(item => `
                         <li>
@@ -34,7 +36,7 @@ export default function(person) {
             </section>
                 ${person.works.length > 0 ? `
                   <section class="content-section container content-person-area activity-area">
-                    <h2 class="content-person-title activity-title section-title pb-2 border-bottom border-light">Достижения</h2>
+                    <h2 class="content-person-title activity-title section-title pb-2 border-bottom border-light">${ translate("Достижения") }</h2>
                 ${person.works.map(work => 
                 `<article class="activity-person-item">
                     <!-- <time class="activity-person-time"></time>-->
