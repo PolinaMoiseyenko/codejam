8e0e1c9dd17dbb58e9fbc7dca61fed6b4968514a
Elena Khlyavskaya <lenach241@gmail.com>
2019-01-28 17:01:37 +0300

    feat: adaptive markup

--- src/components/all/list-item.template.js
+++ src/components/all/list-item.template.js
@@ -3,10 +3,10 @@ import getLink from "./../../utils/getlink";
 export default function(person) {
   return `
    <div class="row py-5">
-    <div class="col-3">
+    <div class="col-md-3 col-12">
       <img src=${person.photo} alt="photograph's picture">
     </div>
-    <div class="col-9 px-5">
+    <div class="col-md-9 col-12 px-5">
       <h3 class="photograph-name"><a href="${getLink('person.html')}&id=${person.id}">${person.name}</a></h3>
     </div>
   </div>`
